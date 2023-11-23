6509f664f06facae2e76bcfdfb6feb68a7c8f301
aromad17 <aromad1117@gmail.com>
2023-11-23 13:55:54 +0900

    header완

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,37 +1,83 @@
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Header() {
-  const [headerOn, setHeaderOn] = useState(false);
-  const [menuOn, setMenuOn] = useState(false);
+  const [headerOn, setHeaderOn] = useState<boolean>(false);
+  const [scrollOn, setScrollOn] = useState<boolean>(false);
   const [menuList, setMenuList] = useState<HTMLElement[]>();
+  const [detailMenu, setDetailMenu] = useState<HTMLElement[]>();
 
   const headerEnter = (): void => {
-    setHeaderOn((prev) => !prev);
+    setHeaderOn(true);
   };
   const headerLeave = (): void => {
-    setHeaderOn((prev) => !prev);
+    setHeaderOn(false);
   };
 
   useEffect(() => {
-    const menuItems = document.querySelectorAll<HTMLLIElement>(".top_menu>li");
-    setMenuList(Array.from(menuItems));
-    console.log(menuList);
+    let winScroll: number = window.scrollY;
+    const myPage: HTMLElement | null = document.querySelector(".mypage");
+    const mySub: HTMLElement | null = document.querySelector(".sub_mypage");
 
-    if (menuList !== undefined) {
-      menuList.forEach((item, idx) => {
-        let itemText = item.parentNode;
-        console.log(itemText);
-        item.addEventListener("mouseenter", () => {
-          menuList[idx].classList.add("mouse_enter");
-        });
-
-        item.addEventListener("mouseleave", () => {
-          menuList[idx].classList.remove("mouse_enter");
-        });
+    if (myPage !== null && mySub !== null) {
+      myPage.addEventListener("mouseenter", () => {
+        mySub.style.display = "block";
+      });
+      myPage.addEventListener("mouseleave", () => {
+        mySub.style.display = "none";
       });
     }
+
+    window.addEventListener("scroll", () => {
+      winScroll = window.scrollY;
+      if (winScroll > 70) {
+        setScrollOn(true);
+        setHeaderOn(true);
+      } else {
+        setScrollOn(false);
+        setHeaderOn(false);
+      }
+    });
+
+    const menuItems = document.querySelectorAll<HTMLLIElement>(".top_menu>li");
+    setMenuList(Array.from(menuItems));
+    const details = document.querySelectorAll<HTMLLIElement>(".detail_menu");
+    setDetailMenu(Array.from(details));
   }, []);
+
+  if (menuList !== undefined && detailMenu !== undefined) {
+    menuList.forEach((item, idx) => {
+      let itemUl = item.parentElement;
+      const headerBackground: HTMLElement | null =
+        document.querySelector(".header_back");
+
+      item.addEventListener("mouseenter", () => {
+        if (itemUl !== null && headerBackground !== null) {
+          itemUl.classList.add("on");
+          headerBackground.classList.add("on");
+          menuList[idx].classList.add("mouse_enter");
+
+          if (detailMenu !== null && idx > 1 && idx < 5) {
+            const detailNum = idx - 2;
+            detailMenu[detailNum].classList.add("on");
+          }
+        }
+      });
+
+      item.addEventListener("mouseleave", () => {
+        if (itemUl !== null && headerBackground !== null) {
+          itemUl.classList.remove("on");
+          headerBackground.classList.remove("on");
+          menuList[idx].classList.remove("mouse_enter");
+
+          if (detailMenu !== null && idx > 1 && idx < 5) {
+            const detailNum = idx - 2;
+            detailMenu[detailNum].classList.remove("on");
+          }
+        }
+      });
+    });
+  }
 
   return (
     <>
@@ -39,7 +85,7 @@ export default function Header() {
       <header
         onMouseEnter={headerEnter}
         onMouseLeave={headerLeave}
-        className={headerOn ? `on` : ""}
+        className={`${headerOn ? "on" : ""} ${scrollOn ? "scrollOn" : ""}`}
       >
         <div className="header_inner">
           <div className="header_menu">
@@ -73,21 +119,195 @@ export default function Header() {
                 <Link legacyBehavior href="/woman" title="우먼">
                   <a>우먼</a>
                 </Link>
+                <div className="detail_menu">
+                  <div className="detail_inner cboth">
+                    <ul>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>신상품</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>베스트</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>전체보기</a>
+                        </Link>
+                      </li>
+                    </ul>
+                    <ul>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>아우터</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>상의</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>하의</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>원피스</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>액세서리</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>이너/언더웨어</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>홈웨어/잠옷</a>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </li>
               <li>
                 <Link legacyBehavior href="/man" title="맨">
                   <a>맨</a>
                 </Link>
+                <div className="detail_menu">
+                  <div className="detail_inner cboth">
+                    <ul>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>신상품</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>베스트</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>전체보기</a>
+                        </Link>
+                      </li>
+                    </ul>
+                    <ul>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>아우터</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>상의</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>하의</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>원피스</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>액세서리</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>이너/언더웨어</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>홈웨어/잠옷</a>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </li>
               <li>
                 <Link legacyBehavior href="/kids" title="키즈">
                   <a>키즈</a>
                 </Link>
+                <div className="detail_menu">
+                  <div className="detail_inner cboth">
+                    <ul>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>신상품</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>베스트</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>전체보기</a>
+                        </Link>
+                      </li>
+                    </ul>
+                    <ul>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>아우터</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>상의</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>하의</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>원피스</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>액세서리</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>이너/언더웨어</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link legacyBehavior href="#">
+                          <a>홈웨어/잠옷</a>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </li>
             </ul>
 
             <ul className="user_menu">
-              <li>
+              <li className="mypage">
                 <Link legacyBehavior href="#">
                   <a>
                     <img
@@ -100,6 +320,18 @@ export default function Header() {
                     />
                   </a>
                 </Link>
+                <ul className="sub_mypage">
+                  <li>
+                    <Link legacyBehavior href="#">
+                      <a>LOGIN</a>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link legacyBehavior href="#">
+                      <a>ORDER</a>
+                    </Link>
+                  </li>
+                </ul>
               </li>
               <li>
                 <Link legacyBehavior href="#">
@@ -149,7 +381,29 @@ export default function Header() {
       </header>
       <style jsx>{`
         .header_back {
-          display: none;
+          position: fixed;
+          top: 0;
+          left: 0;
+          display: block;
+          width: 100%;
+          height: 100%;
+          background: rgba(0, 0, 0, 0);
+          z-index: 0;
+          transition: all 0.3s linear 0s;
+        }
+
+        .cboth {
+        }
+
+        .cboth::after {
+          content: "";
+          display: block;
+          clear: both;
+        }
+
+        .header_back.on {
+          background: rgba(0, 0, 0, 0.7);
+          z-index: 998;
         }
 
         header {
@@ -166,8 +420,15 @@ export default function Header() {
           background-color: #fff;
         }
 
+        header.scrollOn {
+          position: fixed;
+          top: 0;
+          left: 0;
+        }
+
         .header_inner {
           max-width: 1440px;
+          width: 90%;
           height: 100%;
           margin: 0 auto;
         }
@@ -182,12 +443,12 @@ export default function Header() {
           float: left;
         }
 
-        .logo a {
+        .logo > a {
           display: flex;
           align-items: center;
           height: 100%;
         }
-        .logo a img {
+        .logo > a > img {
         }
 
         .header_menu ul.top_menu {
@@ -197,12 +458,12 @@ export default function Header() {
           float: left;
           height: 100%;
         }
-        .header_menu ul.top_menu li {
+        .header_menu > ul.top_menu > li {
           display: inline-block;
           height: 100%;
           padding: 0 20px;
         }
-        .header_menu ul.top_menu li a {
+        .header_menu > ul.top_menu > li > a {
           position: relative;
           display: block;
           height: 100%;
@@ -212,7 +473,7 @@ export default function Header() {
           text-decoration: none;
         }
 
-        .header_menu ul.top_menu li a::after {
+        .header_menu > ul.top_menu > li > a::after {
           content: "";
           display: block;
           position: absolute;
@@ -225,49 +486,209 @@ export default function Header() {
           transition: all 0.3s linear 0s;
         }
 
-        .header_menu ul.top_menu li:hover a::after {
+        .header_menu > ul.top_menu > li:hover > a::after {
           width: 100%;
           opacity: 1;
         }
 
-        .header_menu ul.user_menu {
+        .header_menu > ul.user_menu {
           display: flex;
           align-content: center;
           flex-wrap: wrap;
           float: right;
           height: 100%;
         }
-        .header_menu ul.user_menu li {
-          display: flex;
-          align-content: center;
-          flex-wrap: wrap;
+        .header_menu > ul.user_menu > li {
           display: inline-block;
+          height: 100%;
           margin-right: 32px;
         }
 
-        .header_menu ul.user_menu li:last-child {
+        .header_menu > ul.user_menu > li:first-child {
+          position: relative;
+        }
+
+        .sub_mypage {
+          display: none;
+          position: absolute;
+          top: 90px;
+          left: -30px;
+          width: 90px;
+          padding: 5px 0;
+          border: 1px solid #e3e3e3;
+          z-index: 10;
+          background-color: #fff;
+        }
+        .sub_mypage > li {
+          height: 30px;
+          line-height: 30px;
+          font-size: 13px;
+          text-align: center;
+        }
+        .sub_mypage > li > a {
+          text-decoration: none;
+          color: #555;
+          transition: opacity 0.5s linear 0s;
+          opacity: 0.6;
+        }
+
+        .sub_mypage > li > a:hover {
+          opacity: 1;
+        }
+
+        .header_menu > ul.user_menu > li:last-child {
           margin-right: 0px;
         }
 
-        .header_menu ul.user_menu li a {
+        .header_menu > ul.user_menu > li > a {
           display: block;
+          line-height: 90px;
           color: #fff;
           font-weight: 600;
           text-decoration: none;
         }
 
-        header.on .header_menu ul.top_menu li a {
-          color: #000;
-        }
-        header.on .header_menu ul.top_menu li.mouse_enter a {
-          color: #2e2e2e;
+        header.on .header_menu > ul.user_menu > li > a {
+          transition: opacity 0.3s linear 0s;
+          opacity: 0.5;
         }
 
-        .header_menu ul.top_menu li a.red_color {
+        header.on .header_menu > ul.user_menu > li:hover > a {
+          opacity: 1;
+        }
+
+        .header_menu > ul.top_menu li > .detail_menu {
+          position: absolute;
+          top: -1000px;
+          left: 0;
+          width: 100%;
+          padding: 50px 5% 0 5%;
+          background: #fff;
+          box-sizing: border-box;
+        }
+
+        .header_menu > ul.top_menu li > .detail_menu.on {
+          top: 90px;
+          display: block;
+        }
+
+        .header_menu > ul.top_menu li > .detail_menu > .detail_inner {
+          max-width: 1200px;
+          width: 90%;
+          margin: 0 auto;
+          padding-bottom: 60px;
+          opacity: 0;
+          transition: opacity 0.3s ease-in-out 0s;
+        }
+
+        .header_menu > ul.top_menu li > .detail_menu.on > .detail_inner {
+          opacity: 1;
+        }
+
+        .header_menu > ul.top_menu li > .detail_menu > .detail_inner > ul {
+          float: left;
+        }
+
+        .header_menu
+          > ul.top_menu
+          li
+          > .detail_menu
+          > .detail_inner
+          > ul:first-child {
+          width: 180px;
+        }
+
+        .header_menu
+          > ul.top_menu
+          li
+          > .detail_menu
+          > .detail_inner
+          > ul:first-child
+          > li
+          > a {
+          color: #1a1a1a;
+        }
+
+        .header_menu
+          > ul.top_menu
+          li
+          > .detail_menu
+          > .detail_inner
+          > ul:last-child {
+          width: 200px;
+          padding-left: 40px;
+          border-left: 1px solid #e5e5e5;
+        }
+
+        .header_menu > ul.top_menu li > .detail_menu > .detail_inner > ul > li {
+          height: 30px;
+          line-height: 30px;
+        }
+
+        .header_menu
+          > ul.top_menu
+          li
+          > .detail_menu
+          > .detail_inner
+          > ul
+          > li
+          > a {
+          position: relative;
+          color: #808080;
+          text-decoration: none;
+        }
+
+        .header_menu
+          > ul.top_menu
+          li
+          > .detail_menu
+          > .detail_inner
+          > ul
+          > li
+          > a::after {
+          content: "";
+          display: block;
+          position: absolute;
+          top: 3px;
+          right: 3px;
+          width: 7px;
+          height: 7px;
+          background-color: #c41e20;
+          border-radius: 50%;
+          opacity: 0;
+          transition: all 0.3s ease-in 0s;
+        }
+
+        .header_menu
+          > ul.top_menu
+          li
+          > .detail_menu
+          > .detail_inner
+          > ul
+          > li:hover
+          > a::after {
+          top: 0px;
+          right: -7px;
+          opacity: 1;
+        }
+
+        header.on .header_menu > ul.top_menu > li > a {
+          color: #000;
+        }
+
+        header.on .header_menu > ul.top_menu.on > li > a {
+          opacity: 0.4;
+        }
+
+        header.on .header_menu > ul.top_menu > li.mouse_enter > a {
+          opacity: 1;
+        }
+
+        .header_menu > ul.top_menu > li > a.red_color {
           color: #c41e20;
         }
 
-        header.on .header_menu ul.top_menu li a.red_color {
+        header.on .header_menu > ul.top_menu > li > a.red_color {
           color: #c41e20;
         }
       `}</style>
